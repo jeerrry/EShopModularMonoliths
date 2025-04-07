d254961cccf05cb05011a7306b8b0c93bffec033
Senuch <s.uzairtariq@gmail.com>
2025-04-08 01:51:08 +0500

    Configured Logical CQRS Abstractions

---
+++ src/Shared/Shared/CQRS/ICommandHandler.cs
@@ -0,0 +1,14 @@
+using MediatR;
+
+namespace Shared.CQRS;
+
+public interface ICommandHandler<in TCommand> : ICommandHandler<TCommand, uint>
+    where TCommand : ICommand<uint>
+{
+}
+
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+    where TResponse : notnull
+{
+}
